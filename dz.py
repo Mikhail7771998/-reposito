10f77770a6d536522ef64754fb0f3dddd816d54e
Mikhail7771998 <48120865+Mikhail7771998@users.noreply.github.com>
2023-03-06 16:54:55 +0300

    Add files via upload

---
+++ dz.py
@@ -0,0 +1,327 @@
+# def find_max(spisok):
+#    print(spisok) 
+#    return max(spisok)
+
+# print(find_max([34,54,568,58568,357,426,347,357,358,4,3234,642,-467,565,-68-5,-85,7887,67]))
+# def bool_word(n):
+#    if n == True:
+#       return 'Yes'
+#    else:
+#       return "no"
+         
+# print(bool_word(False))
+# print(sum([436,346,634,634,346,346,3246,624,3246,4]))
+# def sum_two_list(spisok, spisok1):
+#    return sum(spisok+spisok1+spisok+spisok1)
+# print(sum_two_list([2,5,322,5213,],[235,325,15,15331]))
+# def pluus(a, b):
+#    return a + b
+
+
+
+# print(pluus(50,20))
+
+# def rer(a):
+#    return a[-1: -(len(a) + 1): -1]
+# a = 'qwertyuiop'
+# print(rer(a))
+# from re import compile as foo, findall as foo1
+# [4,6,1,print]
+# a = input()
+
+# def func():
+#    print('Hello')
+#    return 'classsssno'
+   
+# def first_func(func):
+#    print(' world')
+#    print(func())
+
+# first_func()
+# a = 3.14
+
+# print(float(a))
+# a = list(map(str, [1,4,5,2,2,4]))
+# print(a)
+# def zag2():
+#    # import re
+#    user_unput = input('сидит в башне принцесса под землей, а волосы у нее сверху')
+#    if user_unput.lower() == 'морковка':
+#       print('Вы молодец, правильно!')
+#    else:
+#       print('вы не молодец, правильный ответ морковка')
+   
+
+# def zag1():
+#    user_unput = input('сорок растежек и сорок застежек..')
+#    if user_unput.lower() == 'капуста':
+#       print('Вы молодец, правильно!')
+#       zag2()
+#    else:
+#       print('вы не молодец, правильный ответ капуста')
+#       zag2()
+
+# zag2()
+
+# b = [1,4,6,7,4,3]
+# print(b.insert(1, 'fhfjfj'))
+# print(b)
+
+
+# def a():
+#     print(1)
+#     print(1)
+#     print(1)
+#     print(1)
+#     print(1)
+#     print(1)
+#     print(1)
+#     print(1)
+#     print(1)
+#     print(1)
+#     return 12
+# x = [1, 5, 7, 7, 5, 3, 2]
+# print(sum(x) / len(x))
+# count = 0
+# for i in range(3, 130):
+#     if '3' in str(i):
+#         count += 1
+
+# print(count)
+# def a():
+#     '''
+#     Это бесполезная функция с ошибкой которая никода не сработает
+#     она вывод результат сложения int с str, что является ошибкой
+#     '''
+#     print(1 + '121312342412')
+
+# print('Хаха, я дпустил тшибку внутри функции, но никто об этом не узнает, потому что я не буду вызывать данную функцию')
+
+# a = [1,1,1,1,1,1,1,1]
+# a = ['hello', 'world', 'apple', 'banana']
+# print('eggs'  not in a)
+
+# def func(spis):
+#     spis += [0,0,0,0,0,0,0,0,0]
+#     print(f'Список который подвергся изенениям {spis}')
+#     return  [i**2 for i in spis]
+# a = [1,4,7,3,13,71]
+# a.remove()
+# print(func(a))
+# print(f'вот так выглядит начальный список а - {a}')
+
+# ban_spis = ['ozon', 'hello', 'kid kod', 'world']
+# filter_ban()
+# def filter_ban(spis):
+#     for e in ban_spis:
+#         if e in spis:
+#             spis.remove(e)
+#     return 
+
+# print(filter_ban(['ozon', 'nikita', 'roma', 'misha']))
+# def test_with():
+#     def __enter__():
+#         pass
+#     def __end__():
+#         pass
+# with open('test.txt', 'r') as f:
+#     a = f.read()
+#     print(a)
+
+# a = ['g64g','g6545e', 'qwerty', 124214124312]
+# b = {'red': 1, 'blue': 56, 'black': 103}
+# b['blue'] = 777
+# print(b)
+
+# a = bool(0)
+# print(a)
+
+# from math import sqrt
+
+# a = sqrt(2)
+# print(a)
+
+# print()input()int()str()tuple()list()bool()float()pop()
+# a = [1,4,3,2,2]
+# b = list('172623637838edbhjidfhjidff')
+# a += ['y', 'o']
+# a.pop(2)
+# print(a)
+# print(b)
+
+# def repeat_three(stroka='Ничего не передали'):
+#     print(stroka * 3)
+
+# def between(start, finish):
+#     return range(start, finish + 1)
+
+
+# print(between(5, 24))
+1
+
+# for i in range(10):
+
+# while True:
+#     if True:
+#         continue
+#     print(1)
+#import math
+
+# def distance(x1, y1, x2, y2):
+#     AB = y1 - y2
+#     AC = x2 - x1
+#     return math.sqrt(AB**2 + AC**2)
+
+# print(distance(2, 0, 3, 5))
+
+# def is_div(x, y, z):
+#     print(y)
+#     print(x)
+#     print(z)
+#     return x % y == 0 and x % z == 0
+
+# print(is_div(4, 2, 7))
+
+
+
+
+# a = [1,3,34,45,5,6,6,5,4,4,4,4,4,4,4,4,4,4]
+# print(sum(a))
+# print(len(a))
+
+# def find_a(0):
+    
+
+
+# #x = [12, 3, 3, 3, 43, 5, 67, 7,5,6,47,67,7,5656,5,5,5,5,568,568,568,82,476132,136,247,846,95,835,,362,47484]
+# find_a([1,3,3,3,2,4])
+# a = [1, 2, 3]
+# a.insert(3, '2342')
+# a.pop(1)
+# v = f'вот так вот {a} {len(a)}'
+# print(v)
+
+# start_list = [1,2,5,1,4,5,5,2,9,7]
+
+# #end_list = [1,2,4,5,7,9]
+
+# def delete_double(arg):
+
+#     result = []
+
+#     for i in arg: 
+#         if i not in result:
+#             result.append(i)
+#         else:
+#             print(f'Ой ты уважаемая цифра {i} уже есть в списке, проваливай')
+
+#     return result
+
+# print(delete_double(start_list))
+
+# class Car:
+
+#     def __init__(self, color, size, oil_type) -> None:
+#         assert type(color) == str
+#         self.color = color
+#         self.size = size
+#         self.oil_type = oil_type
+
+
+#     def drive(self):
+#         print('Машина едет')
+
+#     def right(self):
+#         print('Повернули направо')
+
+#     def light_on(self):
+#         print('свет включен')
+
+
+# lada = Car('Красный', 60, 'Дизель')
+
+# print(f'Ой дорогой! Клянусь, цвет у нее {lada.color} как яблочко, {lada.oil_type} как пикап ест, а маленькая как {lada.size}. ')
+# lada.light_on()
+# lada.drive()
+# lada.right()
+
+# def very_arg(*args):
+#     print(f'эта функция принимает все аоргументы в виде кортежа - вот он - {args}')
+#     for i in args:
+#         print(-i)
+# very_arg(1,4,5,6,5,5,3,32,53,5432,634,26,346,346,346,34,634,643,643,643,6,3246,246,2346,246,426,2456,246,547,213,524,6,3246,246,3246)
+
+# a = [1,2,4,5,2,3,7,8,5,6,3,5]
+
+# def delete_double(spisok):
+#     result = []
+#     # Оператор in : 1 in (2,4,5,6,1,3) --> True
+#     for el_spiska in spisok: # 
+#         if el_spiska not in result:
+#             result.append(el_spiska)
+
+#     return result
+
+# print(delete_double(a))
+# class My_list(list):
+
+#     def __init__(self, spisok):
+#         self.spisok = spisok
+#         super().__init__(self)
+
+        
+#     def __call__(self, *args, **kwds):
+#         print('Ого вызов списка, шок')
+#         # return super().__call__(*args, **kwds)
+        
+# a = My_list([1, 4, 7])
+# a()
+# b = a + [1, 6]
+# print(b)
+# [].pop(5)
+# a = ('hello', 2345678987654321234567898765432, print, 1 + 34 + 23 + 8)
+# print(a)
+# def func(x=0, y=0):
+#     return x ** 2 + y ** 2
+# input('tffctgfuvhuhv')
+# print(func())
+
+# a = []
+# for i in range(len(w))
+#     a.append('_')
+# len(w) 
+
+
+# def polivat_flowers():
+
+#     count_flowers = 100
+
+#     vzyat_leiku()
+#     nalit_water()
+
+#     for i in range(1, count_flowers + 1):
+#         print(f'Поливаем цветок номер {i}')
+
+# def vzyat_leiku():
+#     print('Взяли лейку')
+
+
+
+# def nalit_water():
+#     print('Заполнили лейку водой')
+
+# polivat_flowers()
+
+
+a = 12345887654
+t = 'qwerty'
+b = str(a)
+print(print.__doc__)
+
+def a():
+    '''
+    gvughuguohuogh
+    '''
+    pass
+
+print(a.__doc__)
